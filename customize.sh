5b8d0e1e6e941a1dc49576a2d26e4456f3637227
DanGLES3 <32078304+DanGLES3@users.noreply.github.com>
2019-12-28 23:57:35 -0300

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,13 @@
+  # Don't install on API  # Don't install on API levels other than 29
+  if [ ! $API -eq "29" ]; then
+    abort "This module is for Android 10 only!"
+  fi
+
+if [ -d /system/overlay/NavigationBarModeGestural ]; then
+  mkdir -p $MODPATH/system/overlay
+  cp -rf $MODPATH/overlays/* $MODPATH/system/overlay/
+else
+  mkdir -p $MODPATH/system/product/overlay
+  cp -rf $MODPATH/overlays/* $MODPATH/system/product/overlay/
+fi
+rm -rf $MODPATH/overlays
