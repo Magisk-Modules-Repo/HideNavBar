17492972fe6683e2a5b127347b68d85c82fd3002
DanGLES3 <switchtegra@gmail.com>
2021-01-19 16:39:11 -0300

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,5 +2,5 @@
 if grep 'ro.com.google.ime.kb_pad_port_b' /data/adb/modules/rboard-themes_addon/system.prop /data/adb/modules/gboardnavbar/system.prop; then 
 printf 'do nothing'
 else
-resetprop ro.com.google.ime.kb_pad_port_b 10.0
+resetprop ro.com.google.ime.kb_pad_port_b 1.0
 fi
