dcdf18e5b69713cfa073e5530d2ec4f1ad9c2e97
DanGLES3 <switchtegra@gmail.com>
2021-05-25 17:31:45 -0300

    oops lol

--- common/install.sh
+++ common/install.sh
@@ -75,7 +75,7 @@ fi
 if [ $DBG = true ] ; then
     ui_print " "
     ui_print "   To reenable the back gestures  "
-    ui_print "   adjust the back sensitivity from your phone's gesture settings "
+    ui_print "   adjust the back sensitivity "
     ui_print "   from your phone's gesture settings "
     ui_print " "
 fi    
