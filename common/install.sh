22c13adf635a5070ef044c4daab9065b6a971507
DanGLES3 <switchtegra@gmail.com>
2021-05-25 17:07:56 -0300

    Allow disabling A11-12's back gesture

--- common/install.sh
+++ common/install.sh
@@ -49,6 +49,38 @@ if [ $SS = true ] ; then
      fi
 fi
 
+if [ $API -ge 30 ] ; then
+     ui_print "   Wish to disable the back gesture?  "
+     ui_print " "
+     ui_print "   Vol+ = Yes , Vol- = No "
+     if $VKSEL; then
+     DBG=true
+     else
+     DBG=false
+     fi
+fi     
+
+if [ $DBG = true ] ; then
+     ui_print "   Which side to disable the back gesture?  "
+     ui_print " "
+     ui_print "   Vol+ = Left only (for side menus) , Vol- = Left and Right (for gesture apps) "
+     if $VKSEL; then
+     settings put secure back_gesture_inset_scale_left -1
+     else
+     settings put secure back_gesture_inset_scale_left -1
+     settings put secure back_gesture_inset_scale_right -1
+     fi
+fi
+
+if [ $DBG = true ] ; then
+    ui_print " "
+    ui_print "   To reenable the back gestures  "
+    ui_print "   adjust the back sensitivity from your phone's gesture settings "
+    ui_print "   from your phone's gesture settings "
+    ui_print " "
+fi    
+
+
 OP=$(find /system/overlay /product/overlay /vendor/overlay -type d -iname "navigationbarmodegestural" | cut -d 'N' -f1)
 mkdir -p "$MODPATH"/system"$OP"
 cp -rf "$MODPATH"/Mods/"$VAR"/* "$MODPATH"/Mods/"$VAR3"/* "$MODPATH"/system"$OP"
