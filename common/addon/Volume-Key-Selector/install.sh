6155ff06d514c1a249b5e1db6107b2686819e403
DanGLES3 <switchtegra@gmail.com>
2021-05-29 12:17:25 -0300

    Update install.sh

--- common/addon/Volume-Key-Selector/install.sh
+++ common/addon/Volume-Key-Selector/install.sh
@@ -5,7 +5,7 @@ chmod -R 0755 $MODPATH/common/addon/Volume-Key-Selector/tools
 chooseport() {
   # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
   # Calling it first time detects previous input. Calling it second time will do what we want
-  [ "$1" ] && local delay=$1 || local delay=3
+  [ "$1" ] && local delay=$1 || local delay=10
   local error=false
   while true; do
     timeout 0 $MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32/keycheck
