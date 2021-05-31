fc4d6d2ff3b760332088ccfad2f1c00839966887
DanGLES3 <switchtegra@gmail.com>
2021-05-31 04:14:16 -0300

    Update Vol selector to latest from MMT-EX

--- common/install.sh
+++ common/install.sh
@@ -1,91 +1,56 @@
-   ##########################################################################################
-# Custom Logic
-##########################################################################################
+# External Tools
+chmod -R 0755 $MODPATH/common/addon/Volume-Key-Selector/tools
 
-ui_print "   ----- Fullscreen/Immersive Gestures ------"
-ui_print " "
-ui_print "   Choose your gestures mode   "
-ui_print " "
-ui_print "   Vol+ = Fullscreen, Vol- = Immersive"
-if $VKSEL; then
-     VAR=Q
-     VAR3=a
-     SS=true
-else
-	 VAR=IMQ
-	 SS=true
-fi 
+chooseport_legacy() {
+  # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  [ "$1" ] && local delay=$1 || local delay=3
+  local error=false
+  while true; do
+    timeout 0 $MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32/keycheck
+    timeout $delay $MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32/keycheck
+    local sel=$?
+    if [ $sel -eq 42 ]; then
+      return 0
+    elif [ $sel -eq 41 ]; then
+      return 1
+    elif $error; then
+      abort "Volume key not detected!"
+    else
+      error=true
+      echo "Volume key not detected. Try again"
+    fi
+  done
+}
 
-if [ $VAR = IMQ ] ; then
-     ui_print "   Hide pill and keep keyboard height/space?  "
-     ui_print " "
-     ui_print "   Vol+ = Yes , Vol- = No (default) "
-     if $VKSEL; then
-     VAR3=HP
-     else
-     VAR3=a
-     fi 
-fi
+chooseport() {
+  # Original idea by chainfire and ianmacd @xda-developers
+  [ "$1" ] && local delay=$1 || local delay=10
+  local error=false 
+  while true; do
+    local count=0
+    while true; do
+      timeout $delay /system/bin/getevent -lqc 1 2>&1 > $TMPDIR/events &
+      sleep 0.5; count=$((count + 1))
+      if (`grep -q 'KEY_VOLUMEUP *DOWN' $TMPDIR/events`); then
+        return 0
+      elif (`grep -q 'KEY_VOLUMEDOWN *DOWN' $TMPDIR/events`); then
+        return 1
+      fi
+      [ $count -gt 6 ] && break
+    done
+    if $error; then
+      # abort "Volume key not detected!"
+      echo "Volume key not detected. Trying keycheck method"
+      export chooseport=chooseport_legacy VKSEL=chooseport_legacy
+      chooseport_legacy $delay
+      return $?
+    else
+      error=true
+      echo "Volume key not detected. Try again"
+    fi
+  done
+}
 
-if [ $VAR = IMQ ] ; then
-     ui_print "   Reduce the size of the keyboard bar?  "
-     ui_print " "
-     ui_print "   Vol+ = Yes , Vol- = No (default) "
-     if $VKSEL; then
-     VAR=IMQS
-     else
-     VAR=$VAR
-     fi 
-fi
-
-if [ $SS = true ] ; then
-     ui_print "   Pick the gesture sensitivity.  "
-     ui_print " "
-     ui_print "   Vol+ = Low (Module Default) , Vol- = High (Android's Default) "
-     if $VKSEL; then
-     GIDS=false
-     else
-     VAR="$VAR"DS
-     fi
-fi
-
-if [ $API -ge 30 ] ; then
-     ui_print "   Wish to disable the back gesture?  "
-     ui_print " "
-     ui_print "   Vol+ = Yes , Vol- = No "
-     if $VKSEL; then
-     DBG=true
-     else
-     DBG=false
-     settings delete secure back_gesture_inset_scale_left
-     settings delete secure back_gesture_inset_scale_right
-     fi
-fi     
-
-if [ $DBG = true ] ; then
-     ui_print "   Which side to disable the back gesture?  "
-     ui_print " "
-     ui_print "   Vol+ = Left only (for side menus) , Vol- = Left and Right (for gesture apps) "
-     if $VKSEL; then
-     settings put secure back_gesture_inset_scale_left -1
-     else
-     settings put secure back_gesture_inset_scale_left -1
-     settings put secure back_gesture_inset_scale_right -1
-     fi
-fi
-
-if [ $DBG = true ] ; then
-    ui_print " "
-    ui_print "   To reenable the back gestures  "
-    ui_print "   reinstall module and say no "
-    ui_print "   to disabling the back gesture "
-    ui_print " "
-fi    
-
-
-OP=$(find /system/overlay /product/overlay /vendor/overlay -type d -iname "navigationbarmodegestural" | cut -d 'N' -f1)
-mkdir -p "$MODPATH"/system"$OP"
-cp -rf "$MODPATH"/Mods/"$VAR"/* "$MODPATH"/Mods/"$VAR3"/* "$MODPATH"/system"$OP"
-mv -f  "$MODPATH"/system/system/* "$MODPATH"/system/
-
-rm -rf "$MODPATH"/Mods
+# Keep old variable from previous versions of this
+VKSEL=chooseport
