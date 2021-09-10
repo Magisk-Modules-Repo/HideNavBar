3fe650376420d41cf723e846f8397566867fceae
DanGLES3 <switchtegra@gmail.com>
2021-09-10 18:09:25 -0300

    More reliable Rboard check
    Sorry for the troubles RKBDI

--- service.sh
+++ service.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-if grep 'ro.com.google.ime.kb_pad_port_b' /data/adb/modules/rboard-themes_addon/system.prop /data/adb/modules/gboardnavbar/system.prop; then 
+if [ -f /data/adb/modules/rboard-themes_addon/system.prop ] || [ -f /data/adb/modules/gboardnavbar/system.prop ]; then 
 printf 'do nothing'
 else
 resetprop ro.com.google.ime.kb_pad_port_b 1.0
