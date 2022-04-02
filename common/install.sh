aad22f401d2f2779d2e1d10cbbc2d6a7015244c7
DanGLES3 <switchtegra@gmail.com>
2022-04-02 16:43:50 -0300

    Add files via upload

--- common/install.sh
+++ common/install.sh
@@ -2,34 +2,54 @@
 # Custom Logic
 ##########################################################################################
 
+#Detect and use compatible AAPT
+chmod +x "$MODPATH"/tools/*
+[ "$($MODPATH/tools/aapt v)" ] && AAPT=aapt
+[ "$($MODPATH/tools/aapt64 v)" ] && AAPT=aapt64
+cp -af "$MODPATH"/tools/$AAPT "$MODPATH"/aapt
+cp -rf "$MODPATH"/Mods/QS/* "$MODPATH"/Mods/Qtmp/
+
+
+if [ -d /system/xbin/ ] ; then
+    mkdir -p "$MODPATH"/system/xbin/
+    cp -rf "$MODPATH"/tools/hn "$MODPATH"/system/xbin/
+else
+    mkdir -p "$MODPATH"/system/bin/
+    cp -rf "$MODPATH"/tools/hn "$MODPATH"/system/bin/
+fi 
+
 #Find and delete conflicting overlays
 find /data/adb/modules -type d -not -path "*HideNavBar/system*" -iname "*navigationbarmodegestural*" -exec rm -rf {} \; 2>/dev/null 
 
 #Detect system language for translation 
 LANG=$(settings get system system_locales)
-LANGS=$(echo ${LANG:0:2} )
-if [ -f $MODPATH/Lang/"$LANGS"/"$LANGS"1.txt ]; then
+LANGS=$(echo "${LANG:0:2}" )
+if [ -f "$MODPATH"/Lang/"$LANGS"/"$LANGS"1.txt ]; then
     :
 else
     LANGS=en
 fi
 
+LNG="$MODPATH"/Lang/"$LANGS"/"$LANGS"
+
 #Standard volume selector stuff but with translations
 #Fullscreen or immersive selection
-cat $MODPATH/Lang/"$LANGS"/"$LANGS"1.txt
+cat "$LNG"1.txt
 if $VKSEL; then
-     VAR=Q
+     BH=0.0
+     FH=0.0
+     SS=true
      VAR3=a
      VAR4=a
-     SS=true
 else
-	 VAR=IMQ
-	 SS=true
+     FH=48.0
+     BH=0.0
+     SS=true
 fi 
 
 #Hide pill
-if [ $VAR = IMQ ] ; then
-     cat $MODPATH/Lang/"$LANGS"/"$LANGS"2.txt
+if [ "$FH" = 48.0 ] ; then
+     cat "$LNG"2.txt
      if $VKSEL; then
      VAR3=HP
      else
@@ -38,8 +58,8 @@ if [ $VAR = IMQ ] ; then
 fi
 
 #Hide keyboard buttons 
-if [ $VAR = IMQ ] ; then
-     cat $MODPATH/Lang/"$LANGS"/"$LANGS"8.txt
+if [ "$FH" = 48.0 ] ; then
+     cat "$LNG"8.txt
      if $VKSEL; then
      VAR4=HKB
      else
@@ -48,38 +68,39 @@ if [ $VAR = IMQ ] ; then
 fi
 
 #Small keyboard bar
-if [ $VAR = IMQ ] ; then
-     cat $MODPATH/Lang/"$LANGS"/"$LANGS"3.txt
+if [ "$FH" = 48.0 ] ; then
+     cat "$LNG"3.txt
      if $VKSEL; then
-     VAR=IMQS
+     FH=16.0
      else
-     VAR=$VAR
+     :
      fi 
 fi
 
 #Gesture sensitivity 
-if [ $SS = true ] ; then
-     cat $MODPATH/Lang/"$LANGS"/"$LANGS"4.txt
+if [ "$SS" = true ] ; then
+     cat "$LNG"4.txt
      if $VKSEL; then
-     GIDS=false
+     GS=18.0
      else
-     VAR="$VAR"DS
+     GS=32.0
      fi
 fi
 
 #Disable back gesture on Q
-if [ "$API" -eq 29 ] && [ "$VAR" = Q ] ; then
-     cat $MODPATH/Lang/"$LANGS"/"$LANGS"5.txt
+if [ "$API" -eq 29 ] && [ "$FH" = 0.0 ] ; then
+     cat "$LNG"5.txt
      if $VKSEL; then
      cp -rf "$MODPATH"/Mods/DBGQ/* "$MODPATH"
      else
-     DBG=false
+     :
      fi
 fi
 
 #Disable back gesture on R+
-if [ $API -ge 30 ] ; then
-     cat $MODPATH/Lang/"$LANGS"/"$LANGS"5.txt
+#Reenable back gesture if no is selected 
+if [ "$API" -ge 30 ] ; then
+     cat "$LNG"5.txt
      if $VKSEL; then
      DBG=true
      else
@@ -89,9 +110,9 @@ if [ $API -ge 30 ] ; then
      fi
 fi     
 
-#Reenable back gesture if no is selected 
-if [ $DBG = true ] ; then
-     cat $MODPATH/Lang/"$LANGS"/"$LANGS"6.txt
+#Left or both sides 
+if [ "$DBG" = true ] ; then
+     cat "$LNG"6.txt
      if $VKSEL; then
      settings put secure back_gesture_inset_scale_left -1
      else
@@ -101,16 +122,23 @@ if [ $DBG = true ] ; then
 fi
 
 #Back gesture warning 
-if [ $DBG = true ] ; then
-    cat $MODPATH/Lang/"$LANGS"/"$LANGS"7.txt
+if [ "$DBG" = true ] ; then
+    cat "$LNG"7.txt
 fi    
 
-#Detect original overlay location and install overlays there 
+#Write to overlay resources
+RES="$MODPATH/Mods/Qtmp/res/values/dimens.xml"
+sed -i s/0.3/"$BH"/g "$RES"
+sed -i s/0.1/"$FH"/g "$RES"
+sed -i s/0.2/"$GS"/g "$RES"
+
+#Detect original overlay location
 OP=$(find /system/overlay /product/overlay /vendor/overlay -type d -iname "navigationbarmodegestural" | cut -d 'N' -f1)
 mkdir -p "$MODPATH"/system"$OP"
-cp -rf "$MODPATH"/Mods/"$VAR"/* "$MODPATH"/Mods/"$VAR3"/* "$MODPATH"/Mods/"$VAR4"/* "$MODPATH"/system"$OP"
-mv -f  "$MODPATH"/system/system/* "$MODPATH"/system/
 
-#Junk cleaning
-rm -rf "$MODPATH"/Mods
-rm -rf "$MODPATH"/Lang
+#Build and sign overlays
+"$MODPATH"/aapt p -f -v -M "$MODPATH/Mods/Qtmp/AndroidManifest.xml" -I /system/framework/framework-res.apk -S "$MODPATH/Mods/Qtmp/res" -F "$MODPATH"/unsigned.apk >/dev/null
+"$MODPATH"/tools/zipsigner "$MODPATH"/unsigned.apk "$MODPATH"/Mods/Q/NavigationBarModeGestural/NavigationBarModeGesturalOverlay.apk
+
+#Install overlays 
+cp -rf "$MODPATH"/Mods/Q/* "$MODPATH"/Mods/"$VAR3"/* "$MODPATH"/Mods/"$VAR4"/* "$MODPATH"/system"$OP"
